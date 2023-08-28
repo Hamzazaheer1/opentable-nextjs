21156c53a21a0cd3f51739fdfd2cda0e588438cc
Hamza Zaheer <malikhamza1619619@gmail.com>
2023-08-28 20:10:49 +0500

    4. Defining Page Layouts and Head Meta

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,4 @@
-import Navbar from '../../components/Navbar';
 import Description from './components/Description';
-import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
 import ReservationCard from './components/ReservationCard';
@@ -10,24 +8,18 @@ import Title from './components/Title';
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 text-black min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow text-black">
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg text-black">
+        <ReservationCard />
+      </div>
+    </>
   );
 }
