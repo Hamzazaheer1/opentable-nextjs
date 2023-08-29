0115f2085499083729f70f46e803df9f47b92fad
Hamza Zaheer <malikhamza1619619@gmail.com>
2023-08-29 14:37:50 +0500

    5. Fetching Data in Server Components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,11 +1,36 @@
 import RestaurantNavbar from '../components/RestaurantNavbar';
 import Menu from '../components/Menu';
+import { PrismaClient } from '@prisma/client';
 
-export default function RestaurantMenu() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.items;
+};
+
+export default async function RestaurantMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const menu = await fetchRestaurantMenu(params.slug);
   return (
     <div className="bg-white text-black w-[100%] rounded p-3 shadow">
-      <RestaurantNavbar />
-      <Menu />
+      <RestaurantNavbar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   );
 }
